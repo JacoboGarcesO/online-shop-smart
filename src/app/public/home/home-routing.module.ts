890718301354b2123efbdbf0d4d9c5219cd9a63f
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2024-04-20 17:42:37 -0500

    Create footer

--- src/app/public/home/home-routing.module.ts
+++ src/app/public/home/home-routing.module.ts
@@ -5,6 +5,7 @@ import { HeaderContainerComponent } from '../../containers/header-container/head
 import { WelcomeContainerComponent } from '../../containers/welcome-container/welcome-container.component';
 import { TopProductsContainerComponent } from '../../containers/top-products-container/top-products-container.component';
 import { ReviewsContainerComponent } from '../../containers/reviews-container/reviews-container.component';
+import { FooterContainerComponent } from '../../containers/footer-container/footer-container.component';
 
 const routes: Routes = [
   {
@@ -14,7 +15,8 @@ const routes: Routes = [
       { path: '', component: HeaderContainerComponent, outlet: 'header' },
       { path: '', component: WelcomeContainerComponent, outlet: 'welcome' },
       { path: '', component: TopProductsContainerComponent, outlet: 'top-foods' },
-      { path: '', component: ReviewsContainerComponent, outlet: 'reviews' }
+      { path: '', component: ReviewsContainerComponent, outlet: 'reviews' },
+      { path: '', component: FooterContainerComponent, outlet: 'footer' }
     ]
   }
 ];
